5bc22cd4e16aa0e15e317ffc9dcaa5f3748483fd
shivanand dhanagond <shivananddhanagond48@gmail.com>
2024-04-06 19:40:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,1 @@
-import React from 'react'
-import "./App.css"
 
-import SortingVisualiser from './Sort/SortingVisualiser/SortingVisualiser'
-
-const App = () => {
-  return (
-    <div className="">
-      <SortingVisualiser/>
-    </div>
-  
-  
-  )
-}
-
-export default App
